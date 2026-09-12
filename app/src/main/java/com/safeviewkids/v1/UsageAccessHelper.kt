e0013f57febed24b941c46851946251ce828c875
sanjoypk22 <sanjoypk22@gmail.com>
2026-09-12 22:06:52 +0530

    Update UsageAccessHelper.kt

--- app/src/main/java/com/safeviewkids/v1/UsageAccessHelper.kt
+++ app/src/main/java/com/safeviewkids/v1/UsageAccessHelper.kt
@@ -9,7 +9,8 @@ import android.os.Process
 object UsageAccessHelper {
 
     fun hasUsageAccess(context: Context): Boolean {
-        val appOps = context.getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+        val appOps =
+            context.getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
 
         val mode = appOps.checkOpNoThrow(
             AppOpsManager.OPSTR_GET_USAGE_STATS,
@@ -21,22 +22,34 @@ object UsageAccessHelper {
     }
 
     fun getForegroundPackage(context: Context): String? {
+
+        if (!hasUsageAccess(context)) {
+            return null
+        }
+
         val usageStatsManager =
-            context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+            context.getSystemService(Context.USAGE_STATS_SERVICE)
+                    as UsageStatsManager
 
         val endTime = System.currentTimeMillis()
         val startTime = endTime - 10_000
 
-        val usageEvents = usageStatsManager.queryEvents(startTime, endTime)
+        val usageEvents =
+            usageStatsManager.queryEvents(startTime, endTime)
 
         val event = UsageEvents.Event()
+
         var lastPackage: String? = null
         var lastTime = 0L
 
         while (usageEvents.hasNextEvent()) {
+
             usageEvents.getNextEvent(event)
 
-            if (event.eventType == UsageEvents.Event.MOVE_TO_FOREGROUND) {
+            if (
+                event.eventType ==
+                UsageEvents.Event.MOVE_TO_FOREGROUND
+            ) {
                 if (event.timeStamp > lastTime) {
                     lastTime = event.timeStamp
                     lastPackage = event.packageName
