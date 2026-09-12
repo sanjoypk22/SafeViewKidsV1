35595623556c06ddeb21194e681a14b980b41e99
sanjoypk22 <sanjoypk22@gmail.com>
2026-09-12 08:48:24 +0530

    Add initial settings.gradle.kts configuration

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SafeViewKidsV1"
+include(":app")
